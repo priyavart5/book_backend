63c3b495ae553883ce908bf758734dc3a95acbc2
priyavart10 <priyavart705@gmail.com>
2025-04-17 21:25:16 +0530

    added books functionality

---
+++ src/models/book.model.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from 'mongoose';
+
+const bookSchema = new Schema({
+    title: { 
+        type: String, 
+        required: true 
+    },
+    author: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    user: { 
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User', 
+        required: true 
+    },
+    favourite: {
+        type: Boolean,
+        default: false
+    }
+}, { 
+    timestamps: true 
+});
+
+export default mongoose.model('Book', bookSchema); 
